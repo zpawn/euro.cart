d14714b14206fe546b91d8c201d3140049b5572d
zpawn <ilia.makarov@me.com>
2017-07-18 20:35:16 +0300

    Update vagrant bootstrap

--- vagrant/bootstrap.sh
+++ vagrant/bootstrap.sh
@@ -31,18 +31,20 @@ sudo apt-get install -y php7.0-xml
 sudo apt-get install -y php7.0-zip
 sudo apt-get install -y php7.0-gd
 
-echo ">>> Install composer"
-curl -sS https://getcomposer.org/installer | php
-mv composer.phar /usr/local/bin/composer
-
-echo ">>> Install Composer Asset Plugin"
-composer global require fxp/composer-asset-plugin:~1.3
-
 ### change project dir
 cd /var/www/euro.cart/
 
 echo ">>> current directory"
 pwd
+
+echo ">>> Install composer"
+php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
+php -r "if (hash_file('SHA384', 'composer-setup.php') === '669656bab3166a7aff8a7506b8cb2d1c292f042046c5a994c43155c0be6190fa0355160742ab2e1c88d40d5be660b410') { echo 'Installer verified'; } else { echo 'Installer corrupt'; unlink('composer-setup.php'); } echo PHP_EOL;"
+php composer-setup.php
+php -r "unlink('composer-setup.php');"
+
+echo ">>> Install Composer Asset Plugin"
+php composer.phar global require fxp/composer-asset-plugin:~1.3
 
 # restart services using new config
 sudo service apache2 reload > /dev/null
